c6d931acb3b7c8d3774c7af1007e45d7a4cbd1b9
zilon4eg <hdd500t@gmail.com>
2022-05-19 14:20:59 +0300

    hard mode fix

--- task.js
+++ task.js
@@ -7,47 +7,48 @@ for (const checkbox of allCheckbox) {
             if (downCheckboxList.length > 0) {
                 for (downCheckbox of downCheckboxList) {
                     if (checkbox.checked) {
+                        downCheckbox.indeterminate = false;
                         downCheckbox.checked = true;
                     }
                     else {
+                        downCheckbox.indeterminate = false;
                         downCheckbox.checked = false;
                     }
                 }
             }
             
 
-            if (checkbox.closest('.interests_active') != null) {
-                function checkUp(checkbox) {
-                    if (checkbox.closest('.interests_active') != null) {
+            function checkUp(checkbox) {
+                console.log(checkbox);
+                if (checkbox.closest('.interests_active') != null) {
 
-                        checkboxList = checkbox.closest('.interests_active').querySelectorAll('.interest__check');
-                        let checkboxListCheckedCounter = 0;
+                    checkboxList = checkbox.closest('.interests_active').querySelectorAll('.interest__check');
+                    let checkboxListCheckedCounter = 0;
 
-                        const upCheckbox = checkbox.closest('.interests_active').closest('.interest').querySelector('.interest__check');
+                    const upCheckbox = checkbox.closest('.interests_active').closest('.interest').querySelector('.interest__check');
 
-                        for (checkboxListItem of checkboxList) {
-                            if (checkboxListItem.checked) {
-                                checkboxListCheckedCounter += 1;
-                            }
-                        
-                            if (checkboxList.length === checkboxListCheckedCounter) {
-                                upCheckbox.checked = true;
-                                upCheckbox.indeterminate = false;
-                            }
-                            else if (checkboxListCheckedCounter === 0) {
-                                upCheckbox.indeterminate = false;
-                                upCheckbox.checked = false;
-                            }
-                            else {
-                                upCheckbox.indeterminate = true;
-                            }
+                    for (checkboxListItem of checkboxList) {
+                        if (checkboxListItem.checked) {
+                            checkboxListCheckedCounter += 1;
                         }
-                        checkUp(upCheckbox);
-
+                    
+                        if (checkboxList.length === checkboxListCheckedCounter) {
+                            upCheckbox.checked = true;
+                            upCheckbox.indeterminate = false;
+                        }
+                        else if (checkboxListCheckedCounter === 0) {
+                            upCheckbox.indeterminate = false;
+                            upCheckbox.checked = false;
+                        }
+                        else {
+                            upCheckbox.indeterminate = true;
+                        }
                     }
+                    checkUp(upCheckbox);
+
                 }
-                
-                checkUp(checkbox);                
             }
+            
+            checkUp(checkbox);
         });
     }
